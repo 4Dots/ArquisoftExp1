745260a3319176f6037c15ec6b13c6b6325eec68
estudiante <estudiante@ISIS2503-35.virtual.uniandes.edu.co>
2014-09-12 15:35:41 -0500

    First Commit

---
+++ Experimento1Arquisoft-web/src/main/java/com/FourDots/beans/FacebookSessionBean.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.FourDots.beans;
+
+
+/**
+ *
+ * @author estudiante
+ */
+public class FacebookSessionBean
+{
+
+    /**
+     * Creates a new instance of FacebookSessionBean
+     */
+    public FacebookSessionBean()
+    {
+        
+    }
+
+}
