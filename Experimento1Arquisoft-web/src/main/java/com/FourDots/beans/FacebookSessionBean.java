77e1ce2e6de6ff78705d562aec03ed828b74899c
estudiante <estudiante@ISIS2503-27.virtual.uniandes.edu.co>
2014-09-16 15:13:24 -0500

    Error rarongo con icefaces en el index. hue.

--- Experimento1Arquisoft-web/src/main/java/com/FourDots/beans/FacebookSessionBean.java
+++ Experimento1Arquisoft-web/src/main/java/com/FourDots/beans/FacebookSessionBean.java
@@ -5,20 +5,48 @@
  */
 package com.FourDots.beans;
 
+import org.springframework.social.facebook.api.Facebook;
+import org.springframework.social.facebook.api.PagedList;
+import org.springframework.social.facebook.api.Post;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 
 /**
  *
  * @author estudiante
  */
+@RequestMapping("/")
 public class FacebookSessionBean
 {
 
     /**
+     * 
+     */
+    private Facebook facebook;
+    
+    
+    
+    /**
      * Creates a new instance of FacebookSessionBean
      */
-    public FacebookSessionBean()
+    public FacebookSessionBean(Facebook fb)
     {
-        
+        this.facebook = fb;
     }
 
+    @RequestMapping(method=RequestMethod.GET)
+    public String auth(Model model)
+    {
+        if (!facebook.isAuthorized()) {
+            return "redirect:/connect/facebook";
+        }
+        
+        model.addAttribute(facebook.userOperations().getUserProfile());
+        PagedList<Post> homeFeed = facebook.feedOperations().getHomeFeed();
+        model.addAttribute("feed", homeFeed);
+
+        return "hello";
+    }
 }
